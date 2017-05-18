b85617df176813266b0841adb1cdfccbb37de98a
Nguyen Van Duy <vanduybn95@gmail.com>
2017-05-18 10:38:08 +0700

    Duy

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -10,6 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+/*Route manager category*/
+Route::get('category',array('as'=>'indexCategory','uses'=>'CategoryController@index'));
+Route::get('category/create',array('as'=>'getcreateCategory','uses'=>'CategoryController@getCreate'));
+Route::post('category/create',array('as'=>'postcreateCategory','uses'=>'CategoryController@postCreate'));
+Route::get('category/show/{category}',array('as'=>'showCategory','uses'=>'CategoryController@Show'));
+Route::post('category/show/{category}',array('as'=>'updateCategory','uses'=>'CategoryController@update'));
+Route::get('category/{id}',array('as'=>'destroyCategory','uses'=>'CategoryController@destroy'));
+
 
 Route::get('/', function () {
     return view('/home');
