6f714ad8f23b17ac551fec7ea2a5cb42728cf581
nhamkthd <nhamkthd92@gmail.com>
2017-05-22 20:34:02 +0700

    add edit user info view
    add edit user info view

--- laravel/resources/views/users/info.blade.php
+++ laravel/resources/views/users/info.blade.php
@@ -3,7 +3,12 @@
 @extends('layouts.app')
 
 @section('extend_header')
-	
+	<link href="{{asset('pmd/components/textfield/css/textfield.css')}}" type="text/css" rel="stylesheet"/>
+	<link href="{{asset('pmd/components/checkbox/css/checkbox.css')}}" type="text/css" rel="stylesheet"/>
+	<link href="{{asset('pmd/components/select2/css/select2.min.css')}}" type="text/css" rel="stylesheet"/>
+	<link href="{{asset('pmd/components/select2/css/select2-bootstrap.css')}}" type="text/css" rel="stylesheet"/>
+	<link href="{{asset('pmd/components/select2/css/pmd-select2.css')}}" type="text/css" rel="stylesheet"/>
+	<link href="{{asset('pmd/components/radio/css/radio.css')}}" type="text/css" rel="stylesheet"/>
 @endsection
 
 @section('content')
@@ -21,7 +26,25 @@
 	</div>
 </div>
 
-<!-- Propeller ripple effect js -->
-<script type="text/javascript" src="http://propeller.in/components/button/js/ripple-effect.js"></script>
-<script type="text/javascript" src="{{asset('pmd/components/tetxfield/js/textfield.js')}}"></script>
+<script type="text/javascript" src="{{asset('pmd/components/textfield/js/textfield.js')}}"></script>
+<!-- Propeller checkbox js -->
+<script type="text/javascript" src="http://propeller.in/components/checkbox/js/checkbox.js"></script>
+<!-- Select2 js-->
+<script type="text/javascript" src="http://propeller.in/components/select2/js/select2.full.js"></script>
+<!-- Propeller Select2 -->
+<script type="text/javascript">
+    $(document).ready(function() {
+
+        <!-- Simple Selectbox -->
+        $(".select-simple").select2({
+            theme: "bootstrap",
+            minimumResultsForSearch: Infinity,
+        });
+        
+    });
+</script>
+<!-- Propeller Select2 -->
+<script type="text/javascript" src="http://propeller.in/components/select2/js/pmd-select2.js"></script>
+<!-- Propeller radio js -->
+<script type="text/javascript" src="http://propeller.in/components/radio/js/radio.js"></script>
 @endsection
--- laravel/resources/views/users/info_edit.blade.php
+++ laravel/resources/views/users/info_edit.blade.php
@@ -1,43 +1,123 @@
+{{-- created by nham on 22.05.2017 --}}
+
 @extends('users.info')
 
 @section('info_content')
-	<form class="form-horizontal role="form" method="POST" action="#" style="padding:30px; border:solid 1px green; margin-right: 30px; margin-top: 30px; background: white;">
-		<div class="row">
-			<div class="col-md-5">
-				<div class="form-group pmd-textfield">
-				  	<label for="first-name" class="control-label">
-				   	Ten dang nhap
-				  	</label>
-				  	<input type="text" disabled="" value="{{$user->user_name}}" id="disabled" class="mat-input form-control">
-				</div>
+<form class="form-horizontal role="form" method="POST" action="#" style="padding:30px; border:solid 1px green; margin-right: 30px; margin-top: 30px; background: white;">
+	<div class="row">
+		<div class="col-md-5">
+			<div class="form-group pmd-textfield">
+				<label for="first-name" class="control-label">
+					Ten dang nhap
+				</label>
+				<input type="text" disabled="" value="{{$user->user_name}}" id="disabled" class="mat-input form-control">
 			</div>
-			<div class="col-md-5 offet-md-3">
-				<div class="form-group pmd-textfield">
-				  	<label for="first-name" class="control-label">
-				   	Email
-				  	</label>
-				  	<input type="text" disabled="" value="{{$user->email}}" id="disabled" class="mat-input form-control">
-				</div>
-			</div>
-			<div class="col-md-5">
-				<div class="form-group pmd-textfield">
-					<label for="Small">Ho va ten</label>
-					<input type="text" id="Small" class="form-control" value="{{$user->name}}">
-				</div>
-			</div>
-			<div class="col-md-5 offet-md-3">
-				<div class="form-group pmd-textfield">
-					<label for="Small">Tỉnh/Thành Phố</label>
-					<input type="text" id="Small" class="form-control" value="{{$user->local}}">
-				</div>
-			</div>
-			<div class="col-md-5">
-				<div class="form-group pmd-textfield">
-					<label for="Small">Lớp/Chuyên ngành</label>
-					<input type="text" id="Small" class="form-control" value="{{$user->class}}">
-				</div>	
-			</div>
-
 		</div>
-	</form>
+		<div class="col-md-5 col-md-offset-1">
+			<div class="form-group pmd-textfield">
+				<label for="first-name" class="control-label">
+					Email
+				</label>
+				<input type="text" disabled="" value="{{$user->email}}" id="disabled" class="mat-input form-control">
+			</div>
+		</div>
+		<div class="col-md-5">
+			<div class="form-group pmd-textfield">
+				<label for="Small">Ho va ten</label>
+				<input type="text" id="Small" class="form-control" value="{{$user->name}}">
+			</div>
+		</div>
+		<div class="col-md-5 col-md-offset-1">
+			<div class="form-group pmd-textfield pmd-textfield-floating-label" >       
+				<label>Tỉnh/Thành Phố </label>
+				<select class="select-simple form-control pmd-select2">
+					<option>Hà Nội</option>
+					<option>Hải Phòng</option>
+					<option>TP Hồ Chí Minh</option>
+					<option>Hải Dương</option>
+					<option>Hưng Yên</option>
+					<option>Nam Định</option>
+					<option>Quảng Ninh</option>
+					<option>Đà Nẵng</option>
+					<option>Ninh Bình</option>
+					<option>Thanh Hoá</option>
+					<option>Quảng Bình</option>
+					<option>Phú Yên</option>
+					<option>Hà Giang</option>
+					<option>Bắc Giang</option>
+					<option>Sơn La</option>
+					<option>Điện Biên</option>
+					<option>Lào Cai</option>
+					<option>Yên Bái</option>
+					<option>Quy Nhơn</option>
+					<option>Bến Tre</option>
+					<option>Quảng Nam</option>
+					<option>Nghệ An</option>
+					<option>Thái Bình</option>
+					<option>Khánh Hoà</option>
+				</select>
+			</div>
+		</div>
+		<div class="col-md-5">
+			<div class="form-group pmd-textfield">
+				<label for="Small">Lớp/Chuyên ngành</label>
+				<input type="text" id="Small" class="form-control" value="{{$user->class}}">
+			</div>	
+		</div>
+		<div class="col-md-5 col-md-offset-1">
+			<div class="form-group pmd-textfield pmd-textfield-floating-label">       
+				<label>Năm sinh</label>
+				<select class="select-simple form-control pmd-select2">
+					<option>1970</option>
+					<option>1971</option>
+					<option>1972</option>
+					<option>1973</option>
+					<option>1974</option>
+					<option>1975</option>
+					<option>1976</option>
+					<option>1977</option>
+					<option>1978</option>
+					<option>1979</option>
+					<option>1980</option>
+					<option>1981</option>
+				</select>
+			</div>
+		</div>
+		<div class="col-md-5 ">
+			<div class="form-group pmd-textfield">
+				<label for="Small">Số điện thoại</label>
+				<input type="text" id="Small" class="form-control" value="{{$user->phone}}">
+			</div>	
+		</div>
+		<div class="col-md-5 col-md-offset-1">
+			<div class="form-group pmd-textfield">
+				<label for="Small">ảnh đại diện</label>
+				<input type="file" name="avatar" id="avatar" >
+			</div>	
+		</div>
+
+		<div class="col-md-12">
+			<div class="form-group pmd-textfield">
+			  	<label class="control-label">Giới thiệu bản thân</label>
+			  	<textarea required class="form-control" value"{{$user->desciption}}"></textarea>
+			</div>
+		</div>
+		<div class="col-md-5" style="margin-top: 15px;">
+			<div class="form-group pmd-textfield">
+				<label style="color:#4B515D; margin-top: 15px; margin-right: 20px;" for="Small">Giới tính</label>
+				<label class="radio-inline pmd-radio pmd-radio-ripple-effect">
+					<input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+					<span for="inlineRadio1">Nam</span>
+				</label>
+				<label class="radio-inline pmd-radio pmd-radio-ripple-effect">
+					<input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+					<span for="inlineRadio2">Nữ</span>
+				</label>
+			</div>
+		</div>
+		<div class ="col-md-8 col-md-offset-4" style="margin-top: 20px; margin-bottom: 20px;align:center;">
+                    <button type="submit" id="submit_form" class="btn btn-success">Lưu Lại</button >
+                </div>
+	</div>
+</form>
 @endsection
