0ab0609520383f40ccdc2224df36e08ca538f446
nhamkthd <nhamkthd92@gmail.com>
2017-06-11 16:25:37 +0700

    check edit and delete answers, comments
    check edit and delete answers, comments

--- laravel/app/Answer.php
+++ laravel/app/Answer.php
@@ -24,12 +24,4 @@ class Answer extends Model
    {
       return $this->hasMany('App\voteAnswer','answer_id','id');
    }
-
-  
-   // public function delete()
-   // {
-   //   $this->comments()->delete();
-   //   $this->voteAnswer()->delete();
-   //   parent::delete();
-   // }
 }
--- laravel/app/Http/Controllers/AnswerController.php
+++ laravel/app/Http/Controllers/AnswerController.php
@@ -13,12 +13,19 @@ class AnswerController extends Controller
      public function store(Request $rq)
     {
         $answer=Answer::find(1);
-
     	$answer = new Answer;
     	$answer->user_id = Auth::user()->id;
     	$answer->question_id = $rq->question_id;
     	$answer->content = $rq->content;
     	$answer->save();
         return response()->json($answer);
+    }
+    public function edit(Request $rq)
+    {
+        $answer = Answer::find($rq->answer_id);
+        $answer->content = $rq->answer_content;
+        $answer->save();
+
+        return response()->json($answer);   
     }
 }
