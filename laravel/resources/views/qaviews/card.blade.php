38bdb102fe4cb3445a580d1d70c8077144c8dc77
nhamkthd <nhamkthd92@gmail.com>
2017-06-15 22:54:54 +0700

    udpate views
    udpate views

--- laravel/resources/views/qaviews/card.blade.php
+++ laravel/resources/views/qaviews/card.blade.php
@@ -5,13 +5,16 @@
 		</div>
 		<div class="media-body media-middle">
 			<p class="list-group-item-heading name-text">{{$question->user->user_name}}</p>
-			<p class="sub-text">Đăng {{$question->created_at->diffForHumans()}} tại <span style="color: #e65100 ;"> {{$question->category->title}}</span> | 
+			<p class="sub-text">Đăng {{$question->created_at->diffForHumans()}} tại <span style="color:#e65100;"> 
+			{{$question->category->title}}</span> | 
 			<span class="question-sub-info">
 			<span id="answer_count">{{$question->answers->count()}}</span>&nbsp;<span class="glyphicon glyphicon-comment"></span>&nbsp;&nbsp;
 			<span id="vote_count">{{$question->voteQuestion->count()}} </span>&nbsp;<span class="glyphicon glyphicon-thumbs-up"></span>&nbsp;&nbsp;
 			{{$question->view_count}} <span class="glyphicon glyphicon-eye-open"></span>
 			</span>
 			</p>
+		</div>
+		<div class="question-list-item-info">
 			<a class="question-title" href="{{ url('/qa/show/'.$question->id) }}">{{$question->question_title}}
 				@if($question->is_resolved == true)
 					<span style="color: green;" class="glyphicon glyphicon-ok-sign"></span>
@@ -19,6 +22,7 @@
 					<span style="color: red;" class="glyphicon glyphicon-question-sign"></span>
 				@endif
 			</a>
+			<p>An efficient software developer is one who has a knack for producing what is needed and will be used</p>
 		</div>
 	</div>
 </div>
--- laravel/resources/views/tests/sidebar.blade.php
+++ laravel/resources/views/tests/sidebar.blade.php
@@ -4,9 +4,9 @@
 		<div class="col-md-11 col-md-offset-1 v-sidebar">
 			<p class="v-menu-header">Thống Kê Nhanh<p>
 			<ul class="v-menu-list">
-				<li class="v-menu-item"><a href="{{url('qa')}}"><samp class="glyphicon glyphicon-globe"></samp>&nbsp; Tất cả đề thi</a></li>
+				<li class="v-menu-item"><a href="{{url('tests/')}}"><samp class="glyphicon glyphicon-globe"></samp>&nbsp; Tất cả đề thi</a></li>
 				<li class="v-menu-item"><a href="{{url('qa')}}"><samp class="glyphicon glyphicon-ok"></samp>&nbsp;&nbsp; Đề thi nổi bật</a></li>
-				<li class="v-menu-item"><a href="{{url('qa/myquestion')}}"><span class="glyphicon glyphicon-user"></span>&nbsp;  Đề thi đã tạo</a></li>					<li class="v-menu-item"><a href="#"><span class="glyphicon glyphicon-comment"></span>&nbsp;  Đề thi đã làm</a></li>
+				<li class="v-menu-item"><a href="{{url('tests/user/created')}}"><span class="glyphicon glyphicon-user"></span>&nbsp;  Đề thi đã tạo</a></li>					<li class="v-menu-item"><a href="#"><span class="glyphicon glyphicon-comment"></span>&nbsp;  Đề thi đã làm</a></li>
 				<li class="v-menu-item"><a href="{{url('qa/myfollowing')}}"><span class="glyphicon glyphicon-eye-open"></span>&nbsp;  Đang theo dõi</a></li>
 			</ul>
 			<p class="v-menu-header">Cập nhật<p>
